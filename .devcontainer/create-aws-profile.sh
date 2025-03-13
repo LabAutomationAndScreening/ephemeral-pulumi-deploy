e4cbc561b9aca0de904dd20130258344d437d511
Eli Fine <eli88fine@gmail.com>
2025-03-13 14:12:27 +0000

    copier update

--- .devcontainer/create-aws-profile.sh
+++ .devcontainer/create-aws-profile.sh
@@ -9,14 +9,15 @@ else
   LOCALSTACK_ENDPOINT_URL="http://localstack:4566"
 fi
 
+cat >> ~/.aws/credentials <<EOF
+[localstack]
+aws_access_key_id=test
+aws_secret_access_key=test
+EOF
+
 cat >> ~/.aws/config <<EOF
 [profile localstack]
 region=us-east-1
 output=json
 endpoint_url = $LOCALSTACK_ENDPOINT_URL
-EOF
-cat >> ~/.aws/credentials <<EOF
-[localstack]
-aws_access_key_id=test
-aws_secret_access_key=test
 EOF
